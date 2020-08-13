64cfaf28967e21d4586f370f4b132886aa300b62
Connor Jackson <connorjackson6295@gmail.com>
2020-08-13 16:05:53 -0400

    editing comments

--- applecard.sh
+++ applecard.sh
@@ -4,27 +4,27 @@
 # Steps to run this program:
 # 1. Go into your Wallet app, and click on Card Balance. Select the Statement you want to import.
 # 2. Click Export Transactions and select Comma Separated Values (CSV). Save this file on your iCloud drive so you can access it on your computer.
-# 3. Open the CSV file, and order the columns like: Date - Amount - Merchant - Type
-# 4. Manually edit any poorly formatted merchant names. Manually enter the terms Expense or Income in the Type column.
-# 5. Add a line at the end of the CSV file. Add text like "DONE". SAVE FILE!!!
-# 6. Rename the CSV file as transactions.csv
-# 7. Open Google Chrome and log in to Mint. Go to the transaction page, and open Chromes Inspect tab. In that tab, select Network.
-# 8. Make a test transaction. Before you actually log it, clear the network calls. Add the transaction.
-# 9. Find the network call for updateTransaction.xevent. Right click and select copy -> copy cURL.
-# 10. Inside this code, delete lines 30 through 44 and re-paste with the cURL command you just copied. Keep && echo
-# 11. Inside the CURL command, reference the merchant, date, amount variables. IE merchant='$merchant'&date='$tdate'&amount='$amount'&mtIsExpense='$expense'
-# 12. Save file!!!!!!!!!!!
-# 13. Give terminal permissions to this file by running chmod +x applecard.sh
-# 14. Run script in terminal with ./applecard.sh
-# 15. Make sure your transactions were uploaded properly.
+# 3. Manually edit any poorly formatted merchant names.
+# 4. Rename the CSV file as transactions.csv
+# 5. Open Google Chrome and log in to Mint. Go to the transaction page, and open Chromes Inspect tab. In that tab, select Network.
+# 6. Make a test transaction. Before you actually log it, clear the network calls. Add the transaction.
+# 7. Find the network call for updateTransaction.xevent. Right click and select copy -> copy cURL.
+# 8. Inside this code, delete lines 30 through 44 and re-paste with the cURL command you just copied. Keep && echo
+# 9. Inside the CURL command, reference the merchant, date, amount variables. IE merchant='$merchant'&date='$tdate'&amount='$amount'&mtIsExpense='$expense'
+# 10. Save file!!!!!!!!!!!
+# 11. Give terminal permissions to this file by running chmod +x applecard.sh
+# 12. Run script in terminal with ./applecard.sh
+# 13. Make sure your transactions were uploaded properly.
 
+# By default, Apple Card does not add a newline to the end of the file.
+# in order for all transactions to be read, this newline needs to be added.
 printf "Adding End of Line to CSV file\n"
 echo >> transactions.csv
 
 printf "Start reading file\n\n"
 
 while read p; do
-	# Apple Card CSV's are formatted with comma delimitation.
+  # Apple Card CSV's are formatted with comma delimitation.
   IFS=$',' read -ra COLS <<< "$p"
 
   if [ "${COLS[2]}" == "Description" ]
@@ -37,6 +37,7 @@ while read p; do
   prettydate="${COLS[1]}20"
   tdate="${prettydate//$'/'/%2F}"
 
+  # If we got to the newline at the end, stop running the program.
   if [ "$prettydate" == "20" ]
   then
   	printf "End of file reached: ending\n"
@@ -49,10 +50,17 @@ while read p; do
   # This application takes column 3 to be the merchant, as it's usually the more descriptive value.
   merchant="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${COLS[3]}")"
 
+  # Display data for user
   printf "Query String = %q \n" 'merchant='$merchant'&date='$tdate'&amount='$amount
+  printf "Merchant = " 
+  echo ${COLS[3]}
+  printf "Date = " 
+  echo $prettydate
+  printf "Amount = " 
+  echo $amount
   printf "See cURL response below\n"
 
-  <INSERT CURL COMMAND HERE> && echo
+  #<INSERT CURL COMMAND HERE> && echo
 done <transactions.csv
 
 printf "Complete reading file\n"
